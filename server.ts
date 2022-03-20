142b2432222f67da75813923c02e573fc2a9bd06
Jamie Barton <jamie@notrab.dev>
2022-03-20 07:37:47 +0000

    initial commit

---
+++ server.ts
@@ -0,0 +1,67 @@
+import { createServer } from "@graphql-yoga/node";
+import { useResponseCache } from "@envelop/response-cache";
+
+type User = {
+  id: string;
+  name: string;
+  isAdmin: boolean;
+};
+
+type Post = {
+  id: string;
+  title: string;
+};
+
+const users: User[] = [{ id: "jb", name: "Jamie", isAdmin: true }];
+const posts: Post[] = [{ id: "wtf", title: "Learn GraphQL" }];
+
+const server = createServer({
+  schema: {
+    typeDefs: /* GraphQL */ `
+      type Query {
+        users: [User]!
+        posts: [Post]!
+      }
+
+      type Mutation {
+        updateUser(id: ID!, name: String!): User
+      }
+
+      type User {
+        id: ID!
+        name: String!
+        isAdmin: Boolean!
+      }
+
+      type Post {
+        id: ID!
+        title: String!
+      }
+    `,
+    resolvers: {
+      Query: {
+        users: () => users,
+        posts: () => posts,
+      },
+      Mutation: {
+        updateUser: (_, { id, name }) => {
+          const user = users.find((user) => user.id === id);
+
+          if (!user) {
+            throw new Error("User not found");
+          }
+
+          user.name = name;
+          return user;
+        },
+      },
+    },
+  },
+  plugins: [
+    useResponseCache({
+      includeExtensionMetadata: true,
+    }),
+  ],
+});
+
+server.start();
